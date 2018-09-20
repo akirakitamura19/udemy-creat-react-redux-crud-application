184bbe9f70ba00a60bfef92e34420917cb6251e4
Akira Kitamura <kurukurumoca@gmail.com>
2018-09-20 13:54:34 +0900

    create reducers

---
+++ src/reducers/count.js
@@ -0,0 +1,12 @@
+import INCREMNET, DECREMENT from '../action'
+
+const initialState = { value: 0 }
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { value: state.value + 1}
+    case DECREMENT:
+      return { value: state.value - 1}
+  }
+}
